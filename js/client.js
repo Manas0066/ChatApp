9b3538eaedc98d7a691ee45673845d3f36a5c54c
Manas Chaudhary <manaschaudhary1602@gmail.com>
2024-11-28 04:13:43 +0530

    Update client.js

--- js/client.js
+++ js/client.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:8000');
+const socket = io('https://chat-gxoqi71cg-manas0066s-projects.vercel.app/');
 
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp');
